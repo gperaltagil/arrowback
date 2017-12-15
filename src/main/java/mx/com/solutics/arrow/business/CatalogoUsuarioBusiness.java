1e5c37e66cac87667a2ee0c08f52e5ccdc7fcf0d
gperaltagil <gisel.peralta.gil@gmail.com>
2017-12-15 00:44:36 -0600

    Se agregan los metodos para completar el catalogo de usuarios

--- src/main/java/mx/com/solutics/arrow/business/CatalogoUsuarioBusiness.java
+++ src/main/java/mx/com/solutics/arrow/business/CatalogoUsuarioBusiness.java
@@ -6,10 +6,12 @@
 package mx.com.solutics.arrow.business;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import mx.com.solutics.arrow.bo.estados.CatalogoUsuariosBO;
 import mx.com.solutics.arrow.facade.CatalogoUsuarioFacade;
+import mx.com.solutics.arrow.jpa.CatalogoUsuario;
 
 /**
  *
@@ -21,8 +23,19 @@ public class CatalogoUsuarioBusiness implements Serializable {
     @Inject
     CatalogoUsuarioFacade catalogoUsuarioFacade;
 
-    public CatalogoUsuariosBO agregarUsuario(CatalogoUsuariosBO bO) {
-        return catalogoUsuarioFacade.guardarUsuario(bO);
+    public CatalogoUsuariosBO agregarUsuario(CatalogoUsuario usuario, int tenant) {
+        return catalogoUsuarioFacade.guardarUsuario(usuario, tenant);
+    }
+    
+    public CatalogoUsuariosBO eliminarUsuario(CatalogoUsuario usuario){
+        return catalogoUsuarioFacade.eliminarUsuario(usuario);
     }
 
+    public List<CatalogoUsuariosBO> obtenerUsuariosActivos(){
+        return catalogoUsuarioFacade.obtenerUsuariosActivos();
+    }
+    
+    public CatalogoUsuariosBO obtenerUsuarioID(int id){
+        return catalogoUsuarioFacade.obtenerUsuarioID(id);
+    }
 }
