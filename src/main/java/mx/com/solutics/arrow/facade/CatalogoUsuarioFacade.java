ece8be5e8017ad52fd6362704391108fa556f36e
gperaltagil <gisel.peralta.gil@gmail.com>
2017-12-15 00:44:54 -0600

    Se agregan los metodos para completar el catalogo de usuarios

--- src/main/java/mx/com/solutics/arrow/facade/CatalogoUsuarioFacade.java
+++ src/main/java/mx/com/solutics/arrow/facade/CatalogoUsuarioFacade.java
@@ -6,10 +6,14 @@
 package mx.com.solutics.arrow.facade;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import mx.com.solutics.arrow.bo.estados.CatalogoUsuariosBO;
+import mx.com.solutics.arrow.jpa.CatalogoUsuario;
 import mx.com.solutics.arrow.utils.PersistenciaArrow;
 
 /**
@@ -22,15 +26,34 @@ public class CatalogoUsuarioFacade implements Serializable{
     @Inject
     PersistenciaArrow em;
     
-    public CatalogoUsuariosBO guardarUsuario(CatalogoUsuariosBO bo){
-        bo.getCatalogoUsuario().setEstatus(Boolean.TRUE);
-        bo.getCatalogoUsuario().setFechaIngreso(new Date());
-        if(bo.getCatalogoUsuario().getId() != null){
-            em.getEm().persist(bo.getCatalogoUsuario());
+    public CatalogoUsuariosBO guardarUsuario(CatalogoUsuario usuario, int tenant){
+        usuario.setTenant(tenant);
+        usuario.setEstatus(Boolean.TRUE);
+        usuario.setFechaIngreso(new Date());
+        if(usuario.getId() != null){
+            em.getEm().merge(usuario);
         }else{
-            em.getEm().merge(bo.getCatalogoUsuario());
+            em.getEm().persist(usuario);
         }
         em.getEm().flush();
-        return new CatalogoUsuariosBO(bo.getCatalogoUsuario());
+        return new CatalogoUsuariosBO(usuario);
+    }
+    
+    public CatalogoUsuariosBO eliminarUsuario(CatalogoUsuario usuario){
+        usuario.setEstatus(Boolean.FALSE);
+        em.getEm().merge(usuario);
+        return new CatalogoUsuariosBO(usuario);
+    }
+    
+    public List<CatalogoUsuariosBO> obtenerUsuariosActivos(){
+        List<CatalogoUsuario> usuarios = em.getEm().createQuery("SELECT cu FROM CatalogoUsuario cu where cu.estatus = :activo")
+                .setParameter("activo", true)
+                .getResultList();
+        return new ArrayList<>(usuarios.stream().map(x -> new CatalogoUsuariosBO(x)).collect(Collectors.toList()));
+    }
+    
+    public CatalogoUsuariosBO obtenerUsuarioID(int id){
+        CatalogoUsuario usuario = em.getEm().find(CatalogoUsuario.class, id);
+        return new CatalogoUsuariosBO(usuario);
     }
 }
